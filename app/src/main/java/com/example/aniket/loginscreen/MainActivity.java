2ea2e04e98f9e921a527d7b3ad9ffc035472dbd7
Aniket agarwal <agarwalaniket1999@gmail.com>
2018-01-07 02:58:53 +0530

    I have successfully uploaded my image on the database but still don't know how to retrieve it

--- app/src/main/java/com/example/aniket/loginscreen/MainActivity.java
+++ app/src/main/java/com/example/aniket/loginscreen/MainActivity.java
@@ -2,44 +2,37 @@ package com.example.aniket.loginscreen;
 
 import android.app.ProgressDialog;
 import android.content.Intent;
+import android.graphics.Camera;
 import android.nfc.Tag;
 import android.support.constraint.solver.widgets.Snapshot;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
-import android.text.TextUtils;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
-import android.widget.EditText;
-import android.widget.TextView;
-import android.widget.Toast;
 
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
-import com.google.firebase.messaging.FirebaseMessaging;
-import com.google.firebase.messaging.RemoteMessage;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
-    private Button post;
+    private Button post, camera;
     ProgressDialog progressDialog;
     private DatabaseReference databaseReference;
+    private StorageReference storageReference;
     List <Message> messageList = new ArrayList<>();
     RecyclerView recyclerView;
     RecyclerView.Adapter adapter;
 
-//    private final String TAG = "FCM";
-//    private final String sender_id = "79376285576";
-//    private Random random = new Random();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -48,6 +41,7 @@ public class MainActivity extends AppCompatActivity {
 
         post = (Button) findViewById(R.id.post_button);
         recyclerView = (RecyclerView) findViewById(R.id.recycle_view);
+        camera = (Button) findViewById(R.id.camera_button);
         recyclerView.setHasFixedSize(true);
 
         recyclerView.setLayoutManager(new LinearLayoutManager(MainActivity.this));
@@ -58,6 +52,7 @@ public class MainActivity extends AppCompatActivity {
         progressDialog.show();
 
         databaseReference = FirebaseDatabase.getInstance().getReference("Messages");
+        storageReference = FirebaseStorage.getInstance().getReference("images");
 
         databaseReference.addValueEventListener(new ValueEventListener() {
             @Override
@@ -78,11 +73,22 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        storageReference.getActiveDownloadTasks(new Active)
+
         post.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent open_PostActivity = new Intent(MainActivity.this, PostActivity.class);
                 startActivity(open_PostActivity);
+                finish();
+            }
+        });
+
+        camera.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent camera = new Intent(MainActivity.this, CameraActivity.class);
+                startActivity(camera);
                 finish();
             }
         });
